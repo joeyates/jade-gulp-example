a4a43716c606cdda5412cf0e5c8ed2fdb8a46a16
Joe Yates <joe.g.yates@gmail.com>
2015-01-07 00:49:38 +0100

    Use coffeescript for the gulpfile

---
+++ gulpfile.coffee
@@ -0,0 +1,19 @@
+del = require('del')
+gulp = require('gulp')
+jade = require('gulp-jade')
+
+gulp.task('templates', ->
+  return gulp.src('src/*.jade')
+    .pipe(jade({}))
+    .pipe(gulp.dest('dist/'))
+)
+
+gulp.task('watch', ->
+  gulp.watch('src/*.jade', ['templates'])
+)
+
+gulp.task('clean', (callback) ->
+  del(['dist/*.html'], callback)
+)
+
+gulp.task('default', ['templates', 'watch'])
--- gulpfile.js
+++ gulpfile.js
@@ -1,19 +1,2 @@
-var del = require('del'),
-    gulp = require('gulp'),
-    jade = require('gulp-jade');
-
-gulp.task('templates', function() {
-  return gulp.src('src/*.jade')
-    .pipe(jade({}))
-    .pipe(gulp.dest('dist/'));
-});
-
-gulp.task('watch', function() {
-  gulp.watch('src/*.jade', ['templates']);
-});
-
-gulp.task('clean', function(callback) {
-  del(['dist/*.html'], callback)
-});
-
-gulp.task('default', ['templates', 'watch']);
+require('coffee-script/register');
+require('./gulpfile.coffee');
